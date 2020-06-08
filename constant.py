2cb0d18eeb147b62034a66587704f6b615d351e7
Omkar Bhat <odb6pz@virginia.edu>
2020-06-08 16:55:01 -0400

    Create constant.py

---
+++ constant.py
@@ -0,0 +1,20 @@
+# constants
+adj = ["adaptable", "adventurous", "affectionate", "ambitious", "amiable", "compassionate", "considerate", "courageous",
+       "courteous", "diligent", "empathetic", "exuberant", "frank", "generous", "gregarious", "impartial", "intuitive",
+       "inventive", "passionate", "persistent", "philosophical", "practical", "rational", "reliable", "resourceful",
+       "sensible", "sincere", "sympathetic", "unassuming", "witty", "affable", "affectionate", "agreeable", "ambitious",
+       "amiable", "amicable", "amusing", "brave", "bright", "broad-minded", "calm", "careful", "charming",
+       "communicative", "compassionate", "conscientious", "considerate", "convivial", "courageous", "courteous",
+       "creative", "decisive", "determined", "diligent", "diplomatic", "discreet", "dynamic", "easygoing", "emotional",
+       "energetic", "enthusiastic", "exuberant", "fair-minded", "faithful", "fearless", "forceful", "frank", "friendly",
+       "funny", "generous", "gentle", "good", "gregarious", "hard-working", "helpful", "honest", "humorous",
+       "imaginative", "impartial", "independent", "intellectual", "intelligent", "intuitive", "inventive", "kind",
+       "loving", "loyal", "modest", "neat", "nice", "optimistic", "passionate", "patient", "persistent", "pioneering",
+       "philosophical", "placid", "plucky", "polite", "powerful", "practical", "pro-active", "quick-witted", "quiet",
+       "rational", "reliable", "reserved", "resourceful", "romantic", "self-confident", "self-disciplined", "sensible",
+       "sensitive", "shy", "sincere", "sociable", "straightforward", "sympathetic", "thoughtful", "tidy", "tough",
+       "unassuming", "understanding", "versatile", "warmhearted", "willing", "witty"]
+noun = ["Association", "Band", "Body", "Bunch", "Class", "Cohort", "Company", "Crowd", "Gang", "Group", "Lot", "Mass",
+        "Party", "Posse", "Set", "Society", "Squad", "Staff", "Team", "Tribe", "Group", "Crowd", "Gang",
+        "Squad", "Faculty", "Colony", "onlookers", "Audience", "Jury", "Team", "Army", "Troupe",
+        "Company", "Corporation"]
