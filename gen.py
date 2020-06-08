e0ec3d1ce6c108f8b3aeb19f750afcca1272179a
Omkar Bhat <odb6pz@virginia.edu>
2020-06-08 16:54:59 -0400

    Create gen.py

---
+++ gen.py
@@ -0,0 +1,20 @@
+import constant
+import random
+
+
+def pre_process(a):
+    a = list(set(a))
+    for i in range(len(a)):
+        a[i] = a[i].title()
+    return a
+
+
+def main():
+    adj = pre_process(constant.adj)
+    noun = pre_process(constant.noun)
+    output = str(random.choice(adj)) + " " + str(random.choice(noun))
+    print(output)
+
+
+if __name__ == '__main__':
+    main()
